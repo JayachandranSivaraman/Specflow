337c6daf9977ce60f119f425c3e6ad47fbcda074
Vedula <surya.vedula@honeywell.com>
2017-11-09 13:52:43 +0530

    Specflow initial commit

---
+++ specflow/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("specflow")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Honeywell")]
+[assembly: AssemblyProduct("specflow")]
+[assembly: AssemblyCopyright("Copyright © Honeywell 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("35e25bf8-2ddf-44ff-8d22-ee322b7babd1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
